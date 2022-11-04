e7225d0eb8a989793ad2fcd6474ec2712f82dc84
tkalainoff <t.kalainoff@gmail.com>
2022-11-04 12:04:01 -0400

    edits during office hours

--- aa03A.js
+++ aa03A.js
@@ -11,6 +11,9 @@ var content = fs.readFileSync('data/firstA.json');
 // load by using JSON.parse
 var $ = JSON.parse(content);
 
+var matchIssues = [];
+var latLongs_final =[];
+
 // call function to get lats and longs from the geocode output
 let latLongs = getLatlongs($)
 // console.log(latLongs.length)
@@ -18,7 +21,7 @@ let latLongs = getLatlongs($)
 // call function to reformat original array of addresses to match the formatting from the geocode output
 let addresses = ['109 West 129th Street', '240 West 145th Street', '469 West 142nd Street','240 West 145th Street','109 West 129th Street', '109 West 129th Street']
 let addressesReformatted = reformatAddresses(addresses)
-// console.log(reformatAddresses(addresses))
+console.log(reformatAddresses(addresses))
 
 // call function to merge original array of addresses to the array of objects with addresses and latLongs
 console.log(mergeAddresses(addressesReformatted, latLongs))
@@ -32,29 +35,23 @@ console.log(mergeAddresses(addressesReformatted, latLongs))
   // console.log(latLongs[0]['address'].split(' New York')[0])
   // console.log(addressesAllcaps[0])
 
-
-
 // function to get input address and latlongs from the geocode output Json
 function getLatlongs (array) {
-  // var matchIssues = [];
-  var latLongs =[];
   
   for(let i = 0; i < array.length; i++) {
   let newLatlong = {}
-  
-  newLatlong.address = (array)[i]["InputAddress"]["StreetAddress"]
-  newLatlong.latLong = [(array)[i]["OutputGeocodes"][0]["OutputGeocode"]["Latitude"], (array)[i]["OutputGeocodes"][0]["OutputGeocode"]["Latitude"]]
-    
-  latLongs.push(newLatlong)
 
-    // if ((array)[i]["FeatureMatchingResultType"] !== "Success"){
-    // matchIssues.push(newLatlong)
-    // }
-    // else {
-    // latLongs.push(newLatlong)
-    // }
+    if ((array)[i]["FeatureMatchingResultType"] !== "Success"){
+    matchIssues.push(newLatlong)
+    }
+    else {
+    newLatlong.address = (array)[i]["InputAddress"]["StreetAddress"]
+    newLatlong.latLong = [(array)[i]["OutputGeocodes"][0]["OutputGeocode"]["Latitude"], (array)[i]["OutputGeocodes"][0]["OutputGeocode"]["Latitude"]]
+    console.log(newLatlong)
+    latLongs_final.push(newLatlong)
+    }
   }
-  return latLongs
+  // return latLongs
   // return matchIssues
 }
 
